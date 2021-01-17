44d8cff3e2d28255a41abbb2deca0b456f642b2d
taekhun <nth9708@naver.com>
2021-01-17 23:04:24 +0900

    # naver map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,2 @@
-import { random } from "./util";
-import "./map.js";
-
-const rOne = random(10);
-const rTwo = random(20);
-
-console.log(`${rOne} ${rTwo}`);
+import "./displayMap.js";
+import "./uploadMap.js";
